86316065a7909959b88db491f2f9edfb06ca23b7
michaeldisaro <michael.disaro@gmail.com>
2026-04-03 12:21:20 +0200

    fixed permissions - already applied

--- infra/bootstrapper/prod/data.tf
+++ infra/bootstrapper/prod/data.tf
@@ -9,6 +9,12 @@ data "azurerm_api_management" "ced_apim" {
   resource_group_name = "ced-p-itn-common-rg-01"
 }
 
+data "azurerm_static_web_app" "portal_fe" {
+  provider            = azurerm.PROD-CED
+  name                = "ced-p-itn-portal-fe-stapp-01"
+  resource_group_name = "ced-p-itn-common-rg-01"
+}
+
 data "azuread_group" "admins" {
   display_name = "ced-p-adgroup-admin"
 }

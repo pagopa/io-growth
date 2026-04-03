e6bd8da6de634f7138882bfbb59614c42fcd57ac
michaeldisaro <michael.disaro@gmail.com>
2026-04-03 12:37:12 +0200

    fixed data

--- infra/resources/ced/prod/data.tf
+++ infra/resources/ced/prod/data.tf
@@ -2,6 +2,21 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_subscription" "current" {}
 
+data "azurerm_key_vault" "common" {
+  name                = module.azure_core_values.common_key_vault.name
+  resource_group_name = module.azure_core_values.common_resource_group_name
+}
+
+data "azurerm_key_vault_secret" "action_group_email" {
+  name         = "action-group-email"
+  key_vault_id = data.azurerm_key_vault.common.id
+}
+
+data "azurerm_key_vault_secret" "action_group_slack" {
+  name         = "action-group-slack"
+  key_vault_id = data.azurerm_key_vault.common.id
+}
+
 data "azurerm_private_dns_zone" "managed_redis" {
   name                = "privatelink.redis.azure.net"
   resource_group_name = module.azure_core_values.network_resource_group_name
--- infra/resources/ced/prod/monitor.tf
+++ infra/resources/ced/prod/monitor.tf
@@ -5,13 +5,13 @@ resource "azurerm_monitor_action_group" "ced_error_action_group" {
 
   email_receiver {
     name                    = "email"
-    email_address           = "@Microsoft.KeyVault(VaultName=${module.azure_core_values.common_key_vault.name};SecretName=action-group-email)"
+    email_address           = data.azurerm_key_vault_secret.action_group_email.value
     use_common_alert_schema = true
   }
 
   email_receiver {
     name                    = "slack"
-    email_address           = "@Microsoft.KeyVault(VaultName=${module.azure_core_values.common_key_vault.name};SecretName=action-group-slack)"
+    email_address           = data.azurerm_key_vault_secret.action_group_slack.value
     use_common_alert_schema = true
   }
 

aeeba5a0af1b75986b10811997fdc6101c9b1e64
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2026-04-16 11:52:00 +0200

    chore: remove build issue

--- apps/ced-portal-fe/src/features/session/hooks.ts
+++ apps/ced-portal-fe/src/features/session/hooks.ts
@@ -1,5 +1,4 @@
 import { setCredentials } from '../../core/auth/authSlice';
-import { useToast } from '../../contexts';
 import { useAppDispatch } from '../../hooks/store';
 import { useLazyGetSessionQuery } from './api';
 
@@ -7,14 +6,14 @@ import { useLazyGetSessionQuery } from './api';
 
 export function useAuthorize() {
   const dispatch = useAppDispatch();
-  const { showToast } = useToast();
+  // const { showToast } = useToast();
   const [trigger, result] = useLazyGetSessionQuery();
 
   const authorize = async () => {
     const response = await trigger().unwrap();
     window.localStorage.setItem('STORAGE_KEY', JSON.stringify(response));
     dispatch(setCredentials(response));
-    showToast('Session restored', 'success');
+    // showToast('Session restored', 'success');
     return response;
   };
 

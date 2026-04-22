7f6faeb90f70023188203f8ad6cfcc15484cd930
Krusty93 <andrea.grillo@pagopa.it>
2026-04-22 15:04:10 +0200

    feat: add Azure RM backend configuration for Terraform

---
+++ infra/repository/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ced-p-itn-tfstate-rg-01"
+    storage_account_name = "cedpitntfstatest01"
+    container_name       = "terraform-state"
+    key                  = "ced.repository.tfstate"
+    subscription_id      = "0f1c9857-d58b-47d2-874e-c905c5276dda"
+    use_azuread_auth     = true
+  }
+}

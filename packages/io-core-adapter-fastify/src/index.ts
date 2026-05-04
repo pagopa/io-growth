e171743c4be5714353c0c3369bca3f20654c6ba6
Leonardo De Marco <leodemarco94@gmail.com>
2026-05-04 15:58:18 +0200

    feat(io-core-adapter-fastify): add authenticated validators and response formatters

--- packages/io-core-adapter-fastify/src/index.ts
+++ packages/io-core-adapter-fastify/src/index.ts
@@ -1,12 +1,25 @@
 export {
+  createAuthenticationPreHandler,
+  getSessionFromRequest,
+  type SessionResolver,
+} from "./authenticationPreHandler.js";
+export {
   mapErrorToProblemDetails,
   type ProblemDetails,
   sendErrorResponse,
 } from "./errorMapper.js";
 export {
+  createHttpResponseFormatter,
+  identityFormatter,
+} from "./formatter/httpOutputStandardSchemaFormatter.js";
+export {
   createHttpHandler,
   type HttpHandlerOptions,
 } from "./httpHandlerBuilder.js";
+export {
+  createAuthenticatedInputValidator,
+  createSessionInputValidator,
+} from "./validator/authenticatedInputValidator.js";
 export {
   createHttpRequestValidator,
   emptyValidator,
--- packages/io-core-domain/src/index.ts
+++ packages/io-core-domain/src/index.ts
@@ -3,4 +3,5 @@ export * from "./errors/index.js";
 
 // Export inbound ports
 export type { InputValidator } from "./ports/inbound/inputValidator.js";
+export type { OutputFormatter } from "./ports/inbound/outputFormatter.js";
 export type { UseCase } from "./ports/inbound/use-case.inbound.js";

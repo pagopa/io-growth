b0f922b70ef2a0d0ce5b26616425f440db2cd833
emilio-dimari <emilio.dimari@digitouch.it>
2026-05-07 13:45:31 +0000

    fix: use AppProviders

--- apps/ced-browser-fe/src/main.tsx
+++ apps/ced-browser-fe/src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { AppProviders } from './app/providers';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <AppProviders>
     <App />
-  </BrowserRouter>,
+  </AppProviders>,
 );

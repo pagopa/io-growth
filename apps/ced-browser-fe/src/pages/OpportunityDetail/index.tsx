73f52d01ecdf7c4c669ce616b24e69e0d8f0b436
emilio-dimari <emilio.dimari@digitouch.it>
2026-05-13 08:38:14 +0000

    fix: remove Divider component from section url and access point

--- apps/ced-browser-fe/src/pages/OpportunityDetail/index.tsx
+++ apps/ced-browser-fe/src/pages/OpportunityDetail/index.tsx
@@ -210,8 +210,6 @@ export default function OpportunityDetailPage() {
                     </Box>
                   </Box>
                 </Box>
-
-                <Divider />
               </>
             )}
 

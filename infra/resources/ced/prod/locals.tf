dfcc51c8a566ed3e178d632fb9c857bfbcab0cd9
michaeldisaro <michael.disaro@gmail.com>
2026-05-18 08:21:34 +0000

    fixed card request be settings

--- infra/resources/ced/prod/locals.tf
+++ infra/resources/ced/prod/locals.tf
@@ -77,10 +77,6 @@ locals {
 
     app_settings = {
       PORT            = "8080"
-      POSTGRES_HOST   = "${module.postgresql.postgres.name}.postgres.database.azure.com"
-      POSTGRES_PORT   = "6432"
-      POSTGRES_DB     = azurerm_postgresql_flexible_server_database.ced_test.name
-      POSTGRES_SSL    = "true"
       REDIS_ENDPOINT  = module.redis_dx.endpoint
       REDIS_TLS       = "true"
       AZURE_CLIENT_ID = module.common_container_app_environment.user_assigned_identity.client_id

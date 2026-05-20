e95c90fbe3f0b2ee4c6e777dffa2b98d1e9d6eb7
michaeldisaro <michael.disaro@gmail.com>
2026-05-20 12:09:13 +0000

    updated apim specs

--- infra/resources/ced/_modules/browser_be_container_app/api_browser_be_apim.tf
+++ infra/resources/ced/_modules/browser_be_container_app/api_browser_be_apim.tf
@@ -46,7 +46,7 @@ resource "azurerm_api_management_api" "ced_browser_be_v1" {
 
   import {
     content_format = "openapi-link"
-    content_value  = "https://raw.githubusercontent.com/pagopa/io-growth/refs/heads/main/apps/ced-browser-be/openapi/exposed/openapi.yaml"
+    content_value  = "https://raw.githubusercontent.com/pagopa/io-growth/3c01c94378c4c8aab79bcffeb00e52509572ee48/apps/ced-browser-be/openapi/exposed/openapi.yaml"
   }
 }
 

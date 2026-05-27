de476ce3e907d518b0e350e623cee2fcf6e2b276
emilio-dimari <emilio.dimari@digitouch.it>
2026-05-27 11:04:53 +0000

    fix: update use

--- apps/ced-portal-fe/src/features/benefits/hooks.ts
+++ apps/ced-portal-fe/src/features/benefits/hooks.ts
@@ -1,15 +1,15 @@
 import { useMemo } from 'react';
 import { useGetBenefitsQuery } from './api';
-import type { Benefit, BenefitsQueryParams, OpportunityStatus } from './types';
+import type { Benefit, BenefitsQueryParams } from './types';
 import { OpportunitySummaryItemStatus } from '../../core/api/generated/model/opportunitySummaryItemStatus';
 
-const IN_MANAGEMENT_STATES: Set<OpportunityStatus> = new Set([
+const IN_MANAGEMENT_STATES: Set<OpportunitySummaryItemStatus> = new Set([
   OpportunitySummaryItemStatus.draft,
   OpportunitySummaryItemStatus.test_pending,
   OpportunitySummaryItemStatus.test_passed,
 ]);
 
-const APPROVED_STATES: Set<OpportunityStatus> = new Set([
+const APPROVED_STATES: Set<OpportunitySummaryItemStatus> = new Set([
   OpportunitySummaryItemStatus.published,
   OpportunitySummaryItemStatus.suspended,
   OpportunitySummaryItemStatus.deleted,

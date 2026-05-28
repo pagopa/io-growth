7a6be0d1d28da646cf81b8b58c99d285b8144b39
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2026-05-28 15:28:33 +0200

    chore: remove unused constants

--- apps/ced-portal-fe/src/constants/formOptions/index.ts
+++ apps/ced-portal-fe/src/constants/formOptions/index.ts
@@ -1,7 +1,4 @@
-import {
-  BenefitCategory,
-  PublicationStatus,
-} from '../../features/benefitsFilters/types';
+import { PublicationStatus } from '../../features/benefitsFilters/types';
 import { benefitTypeMap, discountTypeMap } from './types';
 
 const generateOptions = <T extends string>(
@@ -12,25 +9,6 @@ const generateOptions = <T extends string>(
     label,
   }));
 
-export const categoriesOptions = generateOptions(BenefitCategory);
 export const statusOptions = generateOptions(PublicationStatus);
 export const benefitTypeOptions = generateOptions(benefitTypeMap);
 export const fixedPriceBenefitTypeOptions = generateOptions(discountTypeMap);
-
-export const categoriesDropdownDescriptions: Record<
-  keyof typeof BenefitCategory,
-  string
-> = {
-  CULTURE_LEISURE:
-    'Libri, teatro, cinema, concerti, CD, dischi, cibo, bevande, ristoranti, shopping',
-  HEALTH_WELLNESS: 'Negozi di cosmetici, creme, cliniche, SPA, ecc...',
-  EDUCATION: 'Scuole, Università, Corsi di formazione, ecc...',
-  SPORT: 'Negozi di articoli sportivi, strutture sportive, circoli, ecc...',
-  HOME: 'Opportunità per la casa, mutui, gestori della luce e gas, ecc...',
-  TELEPHONY_INTERNET: 'Linea fissa e internet, telefonia mobile, ecc...',
-  FINANCIAL_SERVICES: 'Banche, app di investimenti o di risparmio',
-  TRAVEL_TRANSPORT: 'Agenzie di viaggio, compagnie di trasporti, ecc...',
-  SUSTAINABLE_MOBILITY:
-    'Servizi per muoversi in città, car sharing, monopattini, bici, trasporti green, ecc...',
-  WORK_INTERNSHIPS: 'Concorsi, offerte di lavoro',
-};

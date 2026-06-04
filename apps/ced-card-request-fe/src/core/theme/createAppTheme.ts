30ca2b05f27a1d7c15b9f5f9f541c1fb377dd769
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-06-04 15:43:22 +0200

    chore: removed unused style from theme

--- apps/ced-card-request-fe/src/core/theme/createAppTheme.ts
+++ apps/ced-card-request-fe/src/core/theme/createAppTheme.ts
@@ -48,52 +48,6 @@ export const createAppTheme = () =>
   createTheme(muiItaliaTheme, {
     typography: {
       fontFamily: '"Titillio", Arial, sans-serif',
-      fontWeightLight: 300,
-      fontWeightRegular: 400,
-      fontWeightMedium: 600,
-      fontWeightBold: 700,
-      h1: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '28px',
-        lineHeight: '42px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
-      h2: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '22px',
-        lineHeight: '33px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
-      h3: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '20px',
-        lineHeight: '30px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
-      h4: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '18px',
-        lineHeight: '27px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
-      h5: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '16px',
-        lineHeight: '24px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
-      h6: {
-        fontFamily: '"Titillio", Arial, sans-serif',
-        fontSize: '16px',
-        lineHeight: '24px',
-        letterSpacing: '0px',
-        fontWeight: 600,
-      },
       button: {
         fontFamily: '"Titillio", Arial, sans-serif',
         fontSize: '16px',

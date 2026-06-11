d31f8bddd8c76d2d548b813a1ce6baf60b04a389
michaeldisaro <michael.disaro@gmail.com>
2026-06-11 09:53:37 +0000

    Abstracted service name into infra

--- infra/resources/ced/prod/locals.tf
+++ infra/resources/ced/prod/locals.tf
@@ -25,9 +25,22 @@ locals {
 
     image = "ghcr.io/pagopa/io-growth/ced-portal-be:latest"
 
+    container_app_name = provider::dx::resource_name({
+      prefix          = local.prefix
+      environment     = local.env_short
+      location        = local.location
+      domain          = local.domain
+      name            = "portal-be"
+      instance_number = 1
+      resource_type   = "container_app"
+    })
+
     app_settings = {
       APPLICATIONINSIGHTS_ENTRA_ID_AUTH_ENABLED = "false"
       APPINSIGHTS_SAMPLING_PERCENTAGE           = "100"
+      TELEMETRY_SERVICE_NAME                    = local.portal_be.container_app_name
+
+      AZURE_LOG_LEVEL = "error"
 
       PORT            = "8080"
       POSTGRES_HOST   = "${module.postgresql.postgres.name}.postgres.database.azure.com"

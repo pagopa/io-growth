b787a9186065f8665c42cc0fc2cb3a3e009c4571
Leonardo De Marco <leodemarco94@gmail.com>
2026-06-15 10:38:50 +0200

    fix raw openapi reference

--- infra/resources/ced/_modules/portal_be_container_app/api_portal_be_apim.tf
+++ infra/resources/ced/_modules/portal_be_container_app/api_portal_be_apim.tf
@@ -46,7 +46,7 @@ resource "azurerm_api_management_api" "ced_portal_be_v1" {
 
   import {
     content_format = "openapi-link"
-    content_value  = "https://github.com/pagopa/io-growth/blob/fe9e2d5ca502c0964749d9a6212bd3c179486557/apps/ced-portal-be/openapi/exposed/openapi.yaml"
+    content_value  = "https://raw.githubusercontent.com/pagopa/io-growth/fe9e2d5ca502c0964749d9a6212bd3c179486557/apps/ced-portal-be/openapi/exposed/openapi.yaml"
   }
 }
 

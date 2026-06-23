78a7dd4142b1eec2b1bc645727aa7572927ba064
michaeldisaro <michael.disaro@gmail.com>
2026-06-23 14:50:37 +0000

    added guard to not schedule cron on postgres if on azure

--- apps/ced-portal-be/drizzle/recurrent/R__refresh_materialized_views_cron.sql
+++ apps/ced-portal-be/drizzle/recurrent/R__refresh_materialized_views_cron.sql
@@ -5,9 +5,10 @@
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 -- LOCAL DEVELOPMENT ENVIRONMENT
+-- Only schedule against postgres when neither ced_test nor ced_prod exist.
 DO $$
 BEGIN
-  IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'postgres') THEN
+  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname IN ('ced_test', 'ced_prod')) THEN
     -- refresh materialized views every 15 minutes
     PERFORM cron.schedule_in_database('refresh-place-materialized-view-ced-test', '*/15 * * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY place_materialized_view;', 'postgres');
     PERFORM cron.schedule_in_database('refresh-opportunity-materialized-view-ced-test', '*/15 * * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY opportunity_materialized_view;', 'postgres');

11fab1b33f10a049cc15f413c1e6fc7e62dbec0f
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2026-06-24 15:51:27 +0200

    fix: use error in url field

--- apps/ced-portal-fe/src/pages/CreateBenefit/StepOne/AgreementLinkSection/index.tsx
+++ apps/ced-portal-fe/src/pages/CreateBenefit/StepOne/AgreementLinkSection/index.tsx
@@ -7,12 +7,17 @@ import {
   selectUrl,
 } from '../../../../features/opportunityCreation/selectors';
 import { setField } from '../../../../features/opportunityCreation/opportunityCreationSlice';
+import { isValidHttpsUrl } from '../../../../utils';
 
-export function AgreementLinkSection() {
+export function AgreementLinkSection({
+  attempted,
+}: Readonly<{ attempted: boolean }>) {
   const dispatch = useAppDispatch();
   const activeLanguage = useAppSelector(selectActiveFormLanguage);
   const benefitUrl = useAppSelector(selectUrl);
   const copy = getAgreementCopy(activeLanguage).additionalSections.link;
+
+  const error = isValidHttpsUrl(benefitUrl);
 
   return (
     <Paper elevation={0} sx={{ borderRadius: 2.5, p: { xs: 2, md: 3 } }}>
@@ -25,7 +30,11 @@ export function AgreementLinkSection() {
         <TextField
           disabled={activeLanguage !== 'it'}
           label={copy.benefitUrlLabel}
+          helperText={
+            attempted && error && 'Inserisci un URL valido (es. https://...)'
+          }
           value={benefitUrl}
+          error={attempted && error}
           onChange={(event) =>
             dispatch(
               setField({

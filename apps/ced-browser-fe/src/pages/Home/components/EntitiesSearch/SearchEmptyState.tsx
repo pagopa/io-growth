e5588d89c85534be62a4a7cbabb28e1e6d2db3cb
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-06-27 14:35:15 +0200

    refactor: replace svg with mui component

--- apps/ced-browser-fe/src/pages/Home/components/EntitiesSearch/SearchEmptyState.tsx
+++ apps/ced-browser-fe/src/pages/Home/components/EntitiesSearch/SearchEmptyState.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme } from '@mui/material';
+import { IllusMIError } from '@pagopa/mui-italia';
 
 export function SearchEmptyState() {
   const theme = useTheme();
@@ -11,33 +12,12 @@ export function SearchEmptyState() {
       }}
     >
       <Box sx={{ width: '100%', textAlign: 'center' }}>
-        <svg width="47" height="47" viewBox="0 0 47 47" fill="none">
-          <path
-            d="M23.3333 46.6667C36.22 46.6667 46.6667 36.22 46.6667 23.3333C46.6667 10.4467 36.22 0 23.3333 0C10.4467 0 0 10.4467 0 23.3333C0 36.22 10.4467 46.6667 23.3333 46.6667Z"
-            fill={theme.palette.common.decorativeBlue}
-          />
-          <circle
-            cx="23.3335"
-            cy="30.8003"
-            r="1.86667"
-            fill={theme.palette.common.primaryButton}
-          />
-          <rect
-            x="21.4668"
-            y="14"
-            width="3.73333"
-            height="13.0667"
-            rx="1.86667"
-            fill={theme.palette.common.primaryButton}
-          />
-        </svg>
-
+        <IllusMIError />
         <Typography
           variant="h1"
           component="h1"
           sx={{
             mt: '28px',
-            mb: 1.5,
             color: theme.palette.common.neutral900,
             fontSize: 28,
           }}

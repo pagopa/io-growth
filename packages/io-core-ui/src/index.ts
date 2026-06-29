e2b68bbb62d57db5ec3bc552ecd1bd5ebe05a0c4
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-06-29 11:12:14 +0200

    feat: common mobile spinner

--- packages/io-core-ui/src/index.ts
+++ packages/io-core-ui/src/index.ts
@@ -5,3 +5,4 @@ export {
   LabelCaption,
 } from "./components/Typography/index.js";
 export { HSpacer, VSpacer } from "./components/Spacer/index.js";
+export { MobileSpinnerLoader } from "./components/Loader/index.js";

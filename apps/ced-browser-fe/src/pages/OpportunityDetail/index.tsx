0b2b99dc3370272a96b7e352fb57beb31551b6da
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-06-30 12:29:42 +0200

    chore: add comment

--- apps/ced-browser-fe/src/pages/OpportunityDetail/index.tsx
+++ apps/ced-browser-fe/src/pages/OpportunityDetail/index.tsx
@@ -62,6 +62,7 @@ export default function OpportunityDetailPage() {
           sx={{
             minHeight: '100dvh',
             bgcolor: 'background.paper',
+            // TODO: leave this prop and handle in UI-Core component
             overflowX: 'hidden',
           }}
         >

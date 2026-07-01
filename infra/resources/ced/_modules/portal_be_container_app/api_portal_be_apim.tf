cfc22544201d478fb4460f3d79e1f316a5770bf8
Leonardo De Marco <46130702+leopoldo94@users.noreply.github.com>
2026-07-01 11:49:12 +0200

    update portal-be apim openapi (#291)

--- infra/resources/ced/_modules/portal_be_container_app/api_portal_be_apim.tf
+++ infra/resources/ced/_modules/portal_be_container_app/api_portal_be_apim.tf
@@ -46,7 +46,7 @@ resource "azurerm_api_management_api" "ced_portal_be_v1" {
 
   import {
     content_format = "openapi-link"
-    content_value  = "https://raw.githubusercontent.com/pagopa/io-growth/d74999f4dde82e43f98cd2141bb61bcb5ef21c4e/apps/ced-portal-be/openapi/exposed/openapi.yaml"
+    content_value  = "https://raw.githubusercontent.com/pagopa/io-growth/cb7b221f5e6d3e99744fe6d417b7fba73bbc9091/apps/ced-portal-be/openapi/exposed/openapi.yaml"
   }
 }
 

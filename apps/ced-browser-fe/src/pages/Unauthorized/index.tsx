dce170b5a1287da649ff4cd1e407cb84c43d9f43
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-07-03 11:29:12 +0200

    chore: unauth error screen

--- apps/ced-browser-fe/src/pages/Unauthorized/index.tsx
+++ apps/ced-browser-fe/src/pages/Unauthorized/index.tsx
@@ -1,5 +1,16 @@
+import ErrorScreen from '../../components/QueryGuard/ErrorScreen';
+import { PageErrorType } from '../../components/QueryGuard/ErrorScreen/types';
+
 const Unauthorized = () => {
-  return <div>Unauthorized</div>;
+  return (
+    <ErrorScreen
+      errorType={PageErrorType.UNAUTHORIZED}
+      firstAction={{
+        label: 'Chiudi',
+        onClick: () => window.location.replace('iossoapi://cancel'),
+      }}
+    />
+  );
 };
 
 export default Unauthorized;

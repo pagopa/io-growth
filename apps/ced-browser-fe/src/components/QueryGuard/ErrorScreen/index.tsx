0060c1e7bc7c0fd09d501a27b4d408271e1a4e1c
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-07-03 11:28:43 +0200

    refactor: error screen props

--- apps/ced-browser-fe/src/components/QueryGuard/ErrorScreen/index.tsx
+++ apps/ced-browser-fe/src/components/QueryGuard/ErrorScreen/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Button } from '@mui/material';
 import { Body, Title, VSpacer } from '@pagopa/io-core-ui';
-import { IllusMIMaintenance } from '@pagopa/mui-italia';
-import { useNavigate } from 'react-router-dom';
+import { IllusMIError, IllusMIMaintenance } from '@pagopa/mui-italia';
 import { PageErrorType } from './types';
 
 type ErrorContent = {
@@ -16,16 +15,26 @@ const errorContentMap: Record<PageErrorType, ErrorContent> = {
     title: "Non è stato possibile caricare i dettagli dell'opportunità",
     description: 'Se il problema persiste, riprova in un secondo momento.',
   },
+  [PageErrorType.UNAUTHORIZED]: {
+    icon: <IllusMIError />,
+    title: 'Qualcosa non ha funzionato',
+    description: 'Riprova più tardi.',
+  },
+};
+
+type ButtonProps = {
+  label: string;
+  onClick: () => void;
 };
 
 type Props = {
   errorType: PageErrorType;
-  reloadAction?: () => void;
+  firstAction?: ButtonProps;
+  secondAction?: ButtonProps;
 };
 
-const ErrorScreen = ({ errorType, reloadAction }: Props) => {
+const ErrorScreen = ({ errorType, firstAction, secondAction }: Props) => {
   const { icon, title, description } = errorContentMap[errorType];
-  const navigate = useNavigate();
 
   return (
     <Box
@@ -46,21 +55,27 @@ const ErrorScreen = ({ errorType, reloadAction }: Props) => {
       <VSpacer size={16} />
       <Body>{description}</Body>
       <VSpacer size={16} />
-      {reloadAction && (
+      {firstAction && (
         <>
           <Button
             variant="contained"
-            onClick={reloadAction}
+            onClick={firstAction.onClick}
             sx={{ borderRadius: '10px' }}
           >
-            Riprova
+            {firstAction.label}
           </Button>
           <VSpacer size={8} />
         </>
       )}
-      <Button variant="text" onClick={() => navigate(-1)}>
-        Torna indietro
-      </Button>
+      {secondAction && (
+        <Button
+          variant="text"
+          onClick={secondAction.onClick}
+          sx={{ borderRadius: '10px' }}
+        >
+          {secondAction.label}
+        </Button>
+      )}
     </Box>
   );
 };

e068e070506361e25a4e4bbdfa75d3e38028f331
Leonardo De Marco <leodemarco94@gmail.com>
2026-07-16 10:20:34 +0000

    Wire create draft and upload photo handlers and configure Cosmos DB

--- apps/ced-card-request-be/src/adapters/inbound/fastify/index.ts
+++ apps/ced-card-request-be/src/adapters/inbound/fastify/index.ts
@@ -1,3 +1,5 @@
 export { mountInfoReadinessHandler } from "./health/info-readiness.handler.js";
 export { mountInfoStartupHandler } from "./health/info-startup.handler.js";
+export { mountUploadPhotoHandler } from "./image/upload-photo.handler.js";
+export { mountCreateDraftHandler } from "./request/create-draft.handler.js";
 export { mountGetApplicationStatusHandler } from "./status/get-application-status.handler.js";

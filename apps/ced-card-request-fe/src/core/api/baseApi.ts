df9800700dd34a26fd0968f6a6d9ea226b69e53a
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2026-07-17 11:40:43 +0200

    Revert "chore: baseApi"
    This reverts commit de03be1923e1843f8203b6c39da18e67552a0a37.

--- apps/ced-card-request-fe/src/core/api/baseApi.ts
+++ apps/ced-card-request-fe/src/core/api/baseApi.ts
@@ -2,19 +2,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { selectToken } from '../auth/authSelectors';
 import type { RootState } from '../store';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '/api';
+const MOCKOON_API_BASE_URL = 'http://localhost:3001/api';
 
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: API_BASE_URL,
+    baseUrl: import.meta.env.VITE_API_BASE_URL || MOCKOON_API_BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = selectToken(getState() as RootState);
 
       if (token) {
         headers.set('Authorization', `Bearer ${token}`);
-        const acceptLanguage = 'it';
-        headers.set('Accept-Language', acceptLanguage);
       }
 
       return headers;

bfe2214cf046cda10b951068ed6ca747d49878b4
Pierpaolo Piras <108515572+PierpaoloPirasSp@users.noreply.github.com>
2026-07-20 16:08:46 +0200

    feat: call photo and confirm api | change data format (#325)
    ## Description
    
    This pull request handles an error occurring in the `/request` API
    payload, and calls the `/image` and `/confirm` APIs.
    
    ## How to test
    
    - Complete the first and second steps of the card-request flow, then
    click the **Conferma** button to trigger the `/request` API.
    - Upload a photo and click the **Continua** button to trigger the
    `/image` API.
    - Complete step 4 using any valid data, then confirm the request in the
    final step to trigger the `/confirm` API.

--- apps/ced-card-request-fe/src/features/request-form/selectors.ts
+++ apps/ced-card-request-fe/src/features/request-form/selectors.ts
@@ -1,12 +1,23 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../core/store';
 import { RequestFormState } from './reducer';
+import { parse } from 'date-fns';
 
-const selectRequestSlice = (state: RootState) => state.request;
+const toIsoString = (dateStr: string | undefined) => {
+  if (!dateStr) return '';
+  return parse(dateStr, 'dd/MM/yyyy', new Date()).toISOString();
+};
+
+export const selectRequestForm = (state: RootState) => {
+  const { dataNascita, ...rest } = state.request;
+
+  return {
+    ...rest,
+    dataNascita: toIsoString(dataNascita),
+  };
+};
 
 export const makeSelectRequestFormField = createSelector(
-  selectRequestSlice,
+  selectRequestForm,
   (request) => (field: keyof RequestFormState) => request[field],
 );
-
-export const selectRequestForm = (state: RootState) => state.request;

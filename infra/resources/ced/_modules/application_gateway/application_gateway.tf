b03fff577c209173f400577386c6c277ce22eb38
michaeldisaro <michael.disaro@gmail.com>
2026-07-22 12:55:12 +0000

    moved waf to detection

--- infra/resources/ced/_modules/application_gateway/application_gateway.tf
+++ infra/resources/ced/_modules/application_gateway/application_gateway.tf
@@ -50,10 +50,10 @@ resource "azurerm_web_application_firewall_policy" "this" {
 
   policy_settings {
     enabled                     = true
-    mode                        = "Prevention"
+    mode                        = "Detection"
     request_body_check          = true
     file_upload_limit_in_mb     = 100
-    max_request_body_size_in_kb = 128
+    max_request_body_size_in_kb = 2000
   }
 
   managed_rules {

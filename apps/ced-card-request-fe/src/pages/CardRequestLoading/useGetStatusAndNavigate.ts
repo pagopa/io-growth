28aceb64e8b83a7e26e5e14ba8e6f2d19b175bbc
Pierpaolo Piras <108515572+PierpaoloPirasSp@users.noreply.github.com>
2026-07-23 13:59:58 +0200

    feat: retrieve numDomus from status api (#345)
    ## Description
    
    This pull request handles numDomus retrieved by `/status` api

--- apps/ced-card-request-fe/src/pages/CardRequestLoading/useGetStatusAndNavigate.ts
+++ apps/ced-card-request-fe/src/pages/CardRequestLoading/useGetStatusAndNavigate.ts
@@ -2,9 +2,12 @@ import { useNavigate } from 'react-router-dom';
 import { useLazyGetStatusQuery } from '../../features/read-only-apis/api';
 import { APP_ROUTES } from '../../app/routeConfig';
 import { useEffect } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { setStatusField } from '../../features/status/reducer';
 
 export const useGetStatusAndNavigate = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const [getStatus] = useLazyGetStatusQuery();
 
@@ -26,6 +29,14 @@ export const useGetStatusAndNavigate = () => {
               state: { step: 3 },
             });
           case 'ACQUIRED':
+            if ('numDomus' in status) {
+              dispatch(
+                setStatusField({
+                  field: 'numDomus',
+                  value: String(status.numDomus),
+                }),
+              );
+            }
             // TODO Temporary navigation
             return navigate(APP_ROUTES.REQUEST_SUCCESS, { replace: true });
           default:
@@ -38,5 +49,5 @@ export const useGetStatusAndNavigate = () => {
     };
 
     retrieveStatus();
-  }, [getStatus, navigate]);
+  }, [dispatch, getStatus, navigate]);
 };

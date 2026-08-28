60a1d8cd7479f26840aa9ccfbfc7923d6dcf6c38
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-28 11:51:37 +0200

    refactor: attach persistor with PersistGate

--- apps/ced-card-request-fe/src/app/providers.tsx
+++ apps/ced-card-request-fe/src/app/providers.tsx
@@ -5,21 +5,24 @@ import { AdapterDateFns as AdapterDateFnsV3 } from '@mui/x-date-pickers/AdapterD
 import { it } from 'date-fns/locale';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastProvider } from '../contexts';
-import { store } from '../core/store';
+import { persistor, store } from '../core/store';
 import { appTheme } from '../core/theme/createAppTheme';
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={appTheme}>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterDateFnsV3} adapterLocale={it}>
-          <ToastProvider>
-            <BrowserRouter>{children}</BrowserRouter>
-          </ToastProvider>
-        </LocalizationProvider>
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={appTheme}>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDateFnsV3} adapterLocale={it}>
+            <ToastProvider>
+              <BrowserRouter>{children}</BrowserRouter>
+            </ToastProvider>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }

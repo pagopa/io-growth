0af959fe1d6f1e283e3363913f50d4b7b4b4762a
Leonardo De Marco <leodemarco94@gmail.com>
2026-08-28 09:57:09 +0000

    rename portal-be storage account

--- infra/resources/ced/prod/portal_be_storage.tf
+++ infra/resources/ced/prod/portal_be_storage.tf
@@ -5,7 +5,7 @@ module "portal_be_storage" {
   env_short       = local.env_short
   location        = local.location
   domain          = local.domain
-  app_name        = "portalassets"
+  app_name        = "operatorsassets"
   instance_number = "01"
 
   resource_group_name = azurerm_resource_group.data_rg.name

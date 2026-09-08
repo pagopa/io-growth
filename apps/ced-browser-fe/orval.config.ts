fe2366dba9efe42b0d0393592b48cbd295653df4
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-09-08 09:08:07 +0200

    refactor: set tag for api generator

--- apps/ced-browser-fe/orval.config.ts
+++ apps/ced-browser-fe/orval.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'orval';
 export default defineConfig({
   cedBrowserBe: {
     input: {
-      target: '../ced-browser-be/openapi/exposed/openapi.yaml',
+      target:
+        'https://raw.githubusercontent.com/pagopa/io-growth/ced-browser-be@0.1.14/apps/ced-browser-be/openapi/exposed/openapi.yaml',
     },
     output: {
       client: 'fetch',

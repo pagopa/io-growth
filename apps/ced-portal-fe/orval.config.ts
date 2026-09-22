711fdff65399123554e606ee45b721b3fdd2ee3b
emilio-dimari <emilio.dimari@digitouch.it>
2026-09-22 13:26:27 +0000

    fix: orvarl build error

--- apps/ced-portal-fe/orval.config.ts
+++ apps/ced-portal-fe/orval.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'orval';
 export default defineConfig({
   cedPortalBe: {
     input: {
-      target:
-        'https://raw.githubusercontent.com/pagopa/io-growth/ced-portal-be@0.3.1/apps/ced-portal-be/openapi/exposed/openapi.yaml',
+      target: '../ced-portal-be/openapi/exposed/openapi.yaml',
     },
     output: {
       client: 'fetch',

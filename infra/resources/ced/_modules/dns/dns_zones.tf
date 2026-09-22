b6d4597c189ea0c4f0732df02ecd5aa83e672c9f
michaeldisaro <michael.disaro@gmail.com>
2026-09-22 09:57:00 +0000

    fixed records

--- infra/resources/ced/_modules/dns/dns_zones.tf
+++ infra/resources/ced/_modules/dns/dns_zones.tf
@@ -18,7 +18,7 @@ resource "azurerm_dns_zone" "public" {
 ####################
 
 resource "azurerm_dns_cname_record" "dkim1" {
-  name                = "trwtzdool6n4argcfpo3pz3bvzpstaej._domainkey.ced.pagopa.it"
+  name                = "trwtzdool6n4argcfpo3pz3bvzpstaej._domainkey"
   record               = var.dkim1_record
 
   zone_name           = azurerm_dns_zone.public.name
@@ -30,7 +30,7 @@ resource "azurerm_dns_cname_record" "dkim1" {
 }
 
 resource "azurerm_dns_cname_record" "dkim2" {
-  name                = "u7rskmxwm6bpsh7ukjlonmacce35ygv5._domainkey.ced.pagopa.it"
+  name                = "u7rskmxwm6bpsh7ukjlonmacce35ygv5._domainkey"
   record               = var.dkim2_record
 
   zone_name           = azurerm_dns_zone.public.name
@@ -42,7 +42,7 @@ resource "azurerm_dns_cname_record" "dkim2" {
 }
 
 resource "azurerm_dns_cname_record" "dkim3" {
-  name                = "2oekew7jzmtnpendmmesp6jv37ptf2ak._domainkey.ced.pagopa.it"
+  name                = "2oekew7jzmtnpendmmesp6jv37ptf2ak._domainkey"
   record               = var.dkim3_record
 
   zone_name           = azurerm_dns_zone.public.name
@@ -58,7 +58,7 @@ resource "azurerm_dns_cname_record" "dkim3" {
 ##########################
 
 resource "azurerm_dns_mx_record" "cmf_mx_1" {
-  name                = "bounce.ced.pagopa.it"
+  name                = "bounce"
   zone_name           = azurerm_dns_zone.public.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
@@ -72,7 +72,7 @@ resource "azurerm_dns_mx_record" "cmf_mx_1" {
 }
 
 resource "azurerm_dns_txt_record" "cmf_txt_1" {
-  name                = "bounce.ced.pagopa.it"
+  name                = "bounce"
   zone_name           = azurerm_dns_zone.public.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
@@ -85,7 +85,7 @@ resource "azurerm_dns_txt_record" "cmf_txt_1" {
 }
 
 resource "azurerm_dns_mx_record" "cmf_mx_2" {
-  name                = "bounce.euc1.ced.pagopa.it"
+  name                = "bounce.euc1"
   zone_name           = azurerm_dns_zone.public.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
@@ -99,7 +99,7 @@ resource "azurerm_dns_mx_record" "cmf_mx_2" {
 }
 
 resource "azurerm_dns_txt_record" "cmf_txt_2" {
-  name                = "bounce.euc1.ced.pagopa.it"
+  name                = "bounce.euc1"
   zone_name           = azurerm_dns_zone.public.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
@@ -116,7 +116,7 @@ resource "azurerm_dns_txt_record" "cmf_txt_2" {
 ##################
 
 resource "azurerm_dns_txt_record" "dmarc" {
-  name                = "_dmarc.ced.pagopa.it"
+  name                = "_dmarc"
   zone_name           = azurerm_dns_zone.public.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
